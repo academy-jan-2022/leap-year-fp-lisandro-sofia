85930d1bd5f09b0ae1c81892297676683e65b515
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-17 14:23:29 +0100

    syntax

--- LeapYearFunctional.Tests/Tests.fs
+++ LeapYearFunctional.Tests/Tests.fs
@@ -1,24 +1,22 @@
 module Tests
 open LeapYearFunctional.LeapYear
-open System
 open Xunit
+
+let boolAssertion value =
+  if value then Assert.True else Assert.False
 
 [<Theory>]
 [<InlineData(4, 4, true)>]
 [<InlineData(90, 9, true)>]
 [<InlineData(10, 9, false)>]
 let ``should return true when given number is divisible by divisor`` (number, divisor, expected) =
-    let result =
-        match number with
-        | IsDivisibleBy divisor -> true
-        |_ -> false
-    
-    Assert.Equal(expected, result)
-    
+    match number with
+      | IsDivisibleBy divisor -> true
+      |_ -> false
+    |> boolAssertion expected
+
 [<Theory>]
 [<InlineData(1997, false)>]
 let ``should check if given year is a leap year`` (year, expected) =
-    let result = check year
-    
-    Assert.Equal(expected, result)
-    
+    check year
+    |> boolAssertion expected
