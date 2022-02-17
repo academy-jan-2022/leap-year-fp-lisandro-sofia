ef9524b9805c69f69519957d238ebe9f60800dc4
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-17 14:06:44 +0100

    green

--- LeapYearFunctional/LeapYear.fs
+++ LeapYearFunctional/LeapYear.fs
@@ -1,9 +1,14 @@
 ﻿namespace LeapYearFunctional
 module  LeapYear =
 
-let (|IsDivisibleBy|_|) number divisor =
+let (|IsDivisibleBy|_|) divisor number =
     match number % divisor with
     | 0 -> Some ()
     | _ -> None
+    
+let check year =
+    match year with
+    | IsDivisibleBy 4 -> true
+    |_ -> false
     
     
